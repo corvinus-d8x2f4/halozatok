a0b4c91edaef8c3d2cfe87e533c39a7029903eed
Zalán Lengyel <zalan.lengyel@stud.uni-corvinus.hu>
2021-03-18 11:20:44 +0100

    Statikus fájlok kiszolgálása

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,11 @@
+﻿window.onload = function () {
+document.getElementById("ide").InnerText = 1
+
+var hova = document.getElementById("ide");
+for (var s = 0; s < 10; s++) {
+    let sor = document.createElement("div");
+    hova.appendChild(sor);
+    sor.classList.add("elem")
+    sor.innerText = (s + 1);
+    }
+}
